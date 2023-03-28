36797983f66c03678e54372aaa0c3956a267e5a2
Cole Henley <cole@ournameismud.co.uk>
2023-03-28 15:03:43 +0100

    Initial code commit

---
+++ src/Provider/Signeasy.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Ournameismud\OAuth2\Client\Provider;
+
+use Craft;
+use Exception;
+use InvalidArgumentException;
+use League\OAuth2\Client\Grant\AbstractGrant;
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use Psr\Http\Message\ResponseInterface;
+
+class Signeasy extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+
+    /**
+     * @var string Key used in a token response to identify the resource owner.
+     */
+    const ACCESS_TOKEN_RESOURCE_OWNER_ID = 'id';
+
+    /**
+     * Base domain used for authentication
+     *
+     * @var string
+     */
+    protected $domain = 'https://api.signeasy.com';
+
+    /**
+     * Get authorization url to begin OAuth flow
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return $this->domain . '/oauth2/authorize';
+    }
+
+    /**
+     * Get access token url to retrieve token
+     *
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return $this->domain . '/oauth2/token';
+    }
+
+    /**
+     * Get provider url to fetch user details
+     *
+     * @param  AccessToken $token
+     *
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return $token->getResourceOwnerId();
+    }
+
+    /**
+     * Get the default scopes used by this provider.
+     *
+     * This should not be a complete list of all scopes, but the minimum
+     * required for the provider user interface!
+     *
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+    /**
+     * Retrives the currently configured provider domain.
+     *
+     * @return string
+     */
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+
+
+    protected function getDefaultHeaders() {
+        return array(
+            'Accept'=>'application/json',
+            'Content-type'=>'application/json'
+        );
+    }
+
+
+
+
+    /**
+     * Returns the string that should be used to separate scopes when building
+     * the URL for requesting an access token.
+     *
+     * @return string Scope separator, defaults to ','
+     */
+    protected function getScopeSeparator()
+    {
+        return ' ';
+    }
+
+    /**
+     * Check a provider response for errors.
+     *
+     * @throws IdentityProviderException
+     * @param  ResponseInterface $response
+     * @param  string $data Parsed response data
+     * @return void
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        $statusCode = $response->getStatusCode();
+        if ($statusCode >= 400) {
+            throw new IdentityProviderException(
+                isset($data[0]['message']) ? $data[0]['message'] : $response->getReasonPhrase(),
+                $statusCode,
+                $response
+            );
+        }
+    }
+
+    /**
+     * Generate a user object from a successful user details request.
+     *
+     * @param object $response
+     * @param AccessToken $token
+     * @return SalesforceResourceOwner
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new SalesforceResourceOwner($response);
+    }
+
+    /**
+     * Updates the provider domain with a given value.
+     *
+     * @throws  InvalidArgumentException
+     * @param string $domain
+     * @return  Salesforce
+     */
+    public function setDomain($domain)
+    {
+        try {
+            $this->domain = (string) $domain;
+        } catch (Exception $e) {
+            throw new InvalidArgumentException(
+                'Value provided as domain is not a string'
+            );
+        }
+
+        return $this;
+    }
+}
